b71ac28337f5170531642c03855e6d7230bff87e
sauwming <ming@teluu.com>
2022-11-24 16:01:37 +0800

    Release 2.13

---
+++ docs/2.13/pjsip/docs/html/menudata.js
@@ -0,0 +1,158 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var menudata={children:[
+{text:"Main Page",url:"index.htm"},
+{text:"Related Pages",url:"pages.htm"},
+{text:"Modules",url:"modules.htm"},
+{text:"Namespaces",url:"namespaces.htm",children:[
+{text:"Namespace List",url:"namespaces.htm"},
+{text:"Namespace Members",url:"namespacemembers.htm",children:[
+{text:"All",url:"namespacemembers.htm",children:[
+{text:"a",url:"namespacemembers.htm#index_a"},
+{text:"b",url:"namespacemembers.htm#index_b"},
+{text:"c",url:"namespacemembers.htm#index_c"},
+{text:"g",url:"namespacemembers.htm#index_g"},
+{text:"i",url:"namespacemembers.htm#index_i"},
+{text:"m",url:"namespacemembers.htm#index_m"},
+{text:"r",url:"namespacemembers.htm#index_r"},
+{text:"s",url:"namespacemembers.htm#index_s"},
+{text:"t",url:"namespacemembers.htm#index_t"},
+{text:"v",url:"namespacemembers.htm#index_v"}]},
+{text:"Typedefs",url:"namespacemembers_type.htm",children:[
+{text:"a",url:"namespacemembers_type.htm#index_a"},
+{text:"b",url:"namespacemembers_type.htm#index_b"},
+{text:"c",url:"namespacemembers_type.htm#index_c"},
+{text:"g",url:"namespacemembers_type.htm#index_g"},
+{text:"i",url:"namespacemembers_type.htm#index_i"},
+{text:"m",url:"namespacemembers_type.htm#index_m"},
+{text:"r",url:"namespacemembers_type.htm#index_r"},
+{text:"s",url:"namespacemembers_type.htm#index_s"},
+{text:"t",url:"namespacemembers_type.htm#index_t"},
+{text:"v",url:"namespacemembers_type.htm#index_v"}]},
+{text:"Enumerator",url:"namespacemembers_eval.htm"}]}]},
+{text:"Data Structures",url:"annotated.htm",children:[
+{text:"Data Structures",url:"annotated.htm"},
+{text:"Class Hierarchy",url:"hierarchy.htm"},
+{text:"Data Fields",url:"functions.htm",children:[
+{text:"All",url:"functions.htm",children:[
+{text:"_",url:"functions.htm#index__5F"},
+{text:"a",url:"functions_a.htm#index_a"},
+{text:"b",url:"functions_b.htm#index_b"},
+{text:"c",url:"functions_c.htm#index_c"},
+{text:"d",url:"functions_d.htm#index_d"},
+{text:"e",url:"functions_e.htm#index_e"},
+{text:"f",url:"functions_f.htm#index_f"},
+{text:"g",url:"functions_g.htm#index_g"},
+{text:"h",url:"functions_h.htm#index_h"},
+{text:"i",url:"functions_i.htm#index_i"},
+{text:"j",url:"functions_j.htm#index_j"},
+{text:"k",url:"functions_k.htm#index_k"},
+{text:"l",url:"functions_l.htm#index_l"},
+{text:"m",url:"functions_m.htm#index_m"},
+{text:"n",url:"functions_n.htm#index_n"},
+{text:"o",url:"functions_o.htm#index_o"},
+{text:"p",url:"functions_p.htm#index_p"},
+{text:"q",url:"functions_q.htm#index_q"},
+{text:"r",url:"functions_r.htm#index_r"},
+{text:"s",url:"functions_s.htm#index_s"},
+{text:"t",url:"functions_t.htm#index_t"},
+{text:"u",url:"functions_u.htm#index_u"},
+{text:"v",url:"functions_v.htm#index_v"},
+{text:"w",url:"functions_w.htm#index_w"},
+{text:"x",url:"functions_x.htm#index_x"},
+{text:"y",url:"functions_y.htm#index_y"},
+{text:"z",url:"functions_z.htm#index_z"},
+{text:"~",url:"functions_~.htm#index__7E"}]},
+{text:"Functions",url:"functions_func.htm",children:[
+{text:"a",url:"functions_func.htm#index_a"},
+{text:"b",url:"functions_func_b.htm#index_b"},
+{text:"c",url:"functions_func_c.htm#index_c"},
+{text:"d",url:"functions_func_d.htm#index_d"},
+{text:"e",url:"functions_func_e.htm#index_e"},
+{text:"f",url:"functions_func_f.htm#index_f"},
+{text:"g",url:"functions_func_g.htm#index_g"},
+{text:"h",url:"functions_func_h.htm#index_h"},
+{text:"i",url:"functions_func_i.htm#index_i"},
+{text:"j",url:"functions_func_j.htm#index_j"},
+{text:"l",url:"functions_func_l.htm#index_l"},
+{text:"m",url:"functions_func_m.htm#index_m"},
+{text:"n",url:"functions_func_n.htm#index_n"},
+{text:"o",url:"functions_func_o.htm#index_o"},
+{text:"p",url:"functions_func_p.htm#index_p"},
+{text:"r",url:"functions_func_r.htm#index_r"},
+{text:"s",url:"functions_func_s.htm#index_s"},
+{text:"t",url:"functions_func_t.htm#index_t"},
+{text:"u",url:"functions_func_u.htm#index_u"},
+{text:"v",url:"functions_func_v.htm#index_v"},
+{text:"w",url:"functions_func_w.htm#index_w"},
+{text:"x",url:"functions_func_x.htm#index_x"},
+{text:"~",url:"functions_func_~.htm#index__7E"}]},
+{text:"Variables",url:"functions_vars.htm",children:[
+{text:"_",url:"functions_vars.htm#index__5F"},
+{text:"a",url:"functions_vars_a.htm#index_a"},
+{text:"b",url:"functions_vars_b.htm#index_b"},
+{text:"c",url:"functions_vars_c.htm#index_c"},
+{text:"d",url:"functions_vars_d.htm#index_d"},
+{text:"e",url:"functions_vars_e.htm#index_e"},
+{text:"f",url:"functions_vars_f.htm#index_f"},
+{text:"g",url:"functions_vars_g.htm#index_g"},
+{text:"h",url:"functions_vars_h.htm#index_h"},
+{text:"i",url:"functions_vars_i.htm#index_i"},
+{text:"j",url:"functions_vars_j.htm#index_j"},
+{text:"k",url:"functions_vars_k.htm#index_k"},
+{text:"l",url:"functions_vars_l.htm#index_l"},
+{text:"m",url:"functions_vars_m.htm#index_m"},
+{text:"n",url:"functions_vars_n.htm#index_n"},
+{text:"o",url:"functions_vars_o.htm#index_o"},
+{text:"p",url:"functions_vars_p.htm#index_p"},
+{text:"q",url:"functions_vars_q.htm#index_q"},
+{text:"r",url:"functions_vars_r.htm#index_r"},
+{text:"s",url:"functions_vars_s.htm#index_s"},
+{text:"t",url:"functions_vars_t.htm#index_t"},
+{text:"u",url:"functions_vars_u.htm#index_u"},
+{text:"v",url:"functions_vars_v.htm#index_v"},
+{text:"w",url:"functions_vars_w.htm#index_w"},
+{text:"x",url:"functions_vars_x.htm#index_x"},
+{text:"y",url:"functions_vars_y.htm#index_y"},
+{text:"z",url:"functions_vars_z.htm#index_z"}]}]}]},
+{text:"Files",url:"files.htm",children:[
+{text:"File List",url:"files.htm"},
+{text:"Globals",url:"globals.htm",children:[
+{text:"All",url:"globals.htm",children:[
+{text:"d",url:"globals.htm#index_d"},
+{text:"p",url:"globals_p.htm#index_p"}]},
+{text:"Functions",url:"globals_func.htm",children:[
+{text:"p",url:"globals_func.htm#index_p"}]},
+{text:"Variables",url:"globals_vars.htm",children:[
+{text:"p",url:"globals_vars.htm#index_p"}]},
+{text:"Typedefs",url:"globals_type.htm",children:[
+{text:"p",url:"globals_type.htm#index_p"}]},
+{text:"Enumerations",url:"globals_enum.htm",children:[
+{text:"p",url:"globals_enum.htm#index_p"}]},
+{text:"Enumerator",url:"globals_eval.htm",children:[
+{text:"p",url:"globals_eval.htm#index_p"}]},
+{text:"Macros",url:"globals_defs.htm",children:[
+{text:"d",url:"globals_defs.htm#index_d"},
+{text:"p",url:"globals_defs_p.htm#index_p"}]}]}]}]}
